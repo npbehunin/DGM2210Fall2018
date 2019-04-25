74ed891246bffe13cbdd33b278bbd75ea48bea97
Nathan <npbehunin@gmail.com>
2019-04-25 14:10:52 -0600

    Added new scene with new fox model fbx

---
+++ DGM2210ModelingClass/Assets/Fox Model/Fox Model (New)/FoxMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Animator animator;
+    public float MoveSpeed;
+    
+    public bool canMove;
+    
+    void Start()
+    {
+        canMove = false;
+        StartCoroutine(moveForward());
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (canMove)
+        {
+            rb.MovePosition(transform.position + -transform.right * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private IEnumerator moveForward()
+    {
+        yield return new WaitForSeconds(2);
+        canMove = true;
+        animator.SetBool("Walking", true);
+        yield return new WaitForSeconds(3);
+        canMove = false;
+        animator.SetBool("Walking", false);
+    }
+}
